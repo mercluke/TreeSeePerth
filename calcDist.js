8e61922613d3d27a181256adf24288007385d62b
A-Antulov <ashtyn.antulov@gmail.com>
2017-07-29 14:03:36 +0800

    Changed to a class

--- calcDist.js
+++ calcDist.js
@@ -1,17 +1,24 @@
-function calcDistKm(lat1, lon1, lat2, lon2)
+class CalcDist
 {
-  var rad = 6371; //Radius of the earth in km
-  var dLat = degToRad(lat2 - lat1);
-  var dLon = degToRad(lon2 - lon1);
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
-          Math.sin(dLon/2) * Math.sin(dLon/2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  // haha I don't know how to use JavaScript.. or Python, but I guess that is
+  // why you're taking FOP next sem? :D
 
-  return rad * c;
-}
+  function calcDistKm(lat1, lon1, lat2, lon2)
+  {
+    var rad = 6371; //Radius of the earth in km
+    var dLat = degToRad(lat2 - lat1);
+    var dLon = degToRad(lon2 - lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-function degToRad(deg)
-{
-  return deg * (Math.PI/180);
+    return rad * c;
+  }
+
+  function degToRad(deg)
+  {
+    return deg * (Math.PI/180);
+  }
+
 }
