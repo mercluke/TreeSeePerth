6d3e260e365a24be70768ad4e9d937476a3641a8
Other USer <otheruser@MacBook-Pro-2.local>
2017-07-29 11:59:56 +0800

    function to calculate distance between 2 points

---
+++ calcDist.js
@@ -0,0 +1,17 @@
+function calcDistKm(lat1, lon1, lat2, lon2)
+{
+  var rad = 6371; //Radius of the earth in km
+  var dLat = degToRad(lat2 - lat1);
+  var dLon = degToRad(lon2 - lon1);
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return rad * c;
+}
+
+function degToRad(deg)
+{
+  return deg * (Math.PI/180);
+}
